c91f23ec65b06cb10eabb8170f261bb40af7834e
Kristina Boratkova <k.boratkova@gmail.com>
2020-04-08 21:20:41 +0200

    pocet prvkov v poli a rozpatie

---
+++ zadanie1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+main(){
+	int pocet, min, max;
+	printf("Zadajte kolko ma byt prvkov v poli:");
+	scanf("%d", &pocet);
+	printf("Zadajte rozpatie pri nahodnych hodnotach:");
+	printf("Minimum:");
+	scanf("%d", &min);
+	printf("Maximum:");
+	scanf("%d", &max);
+	
+	}
